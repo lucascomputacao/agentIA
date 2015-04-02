8d9dff3d9cb407296abb81e4dfd12a96d5b70405
Lucas Borges <lucascomputacao@gmail.com>
2015-04-02 17:33:27 -0300

    first commit

---
+++ agentIA.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   agentIA.c
+ * Author: Sammy Guergachi <sguergachi at gmail.com>
+ *
+ * Created on 2 de Abril de 2015, 17:29
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
