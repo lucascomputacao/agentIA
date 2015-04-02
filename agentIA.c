7472b07185642671c37924eb1bacaf95de82f629
Lucas Borges <lucascomputacao@gmail.com>
2015-04-02 17:39:20 -0300

    Iniciando projeto do Agente Inteligente para localizar as vagas mais próximas da entrada no estacionamento e indicá-las ao motorista que chega ao local

--- agentIA.c
+++ agentIA.c
@@ -1,6 +1,9 @@
 /* 
  * File:   agentIA.c
- * Author: Sammy Guergachi <sguergachi at gmail.com>
+ * Author: Igor Paim, Lucas Borges, Tiago Pacheco
+ * 
+ * Projeto de Agente Inteligente para localizar as vagas mais próximas da entrada
+ * no estacionamento e indicar ao motorista que chega ao local
  *
  * Created on 2 de Abril de 2015, 17:29
  */
